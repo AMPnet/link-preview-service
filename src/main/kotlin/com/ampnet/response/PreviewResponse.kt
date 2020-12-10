f9f71a5e2ed5ac35b53fadcd0fe0ade52cdd0fb5
Eugen Druzin <edruzin@gmail.com>
2020-12-10 12:17:16 +0100

    Return webiste url on invalid OpenGraph data

--- src/main/kotlin/com/ampnet/response/PreviewResponse.kt
+++ src/main/kotlin/com/ampnet/response/PreviewResponse.kt
@@ -1,4 +1,10 @@
 package com.ampnet.response
 
-data class PreviewResponse(val title: String, val description: String, val image: ImagePreviewResponse, val url: String)
-data class ImagePreviewResponse(val url: String, val height: String, val width: String)
+data class PreviewResponse(
+    val url: String,
+    val title: String? = null,
+    val description: String? = null,
+    val image: ImagePreviewResponse? = null
+)
+
+data class ImagePreviewResponse(val url: String?, val height: String?, val width: String?)
